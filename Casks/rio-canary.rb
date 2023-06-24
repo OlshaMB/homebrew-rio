57de5cfe31a6554bdcaf82c27bba1daa4aef49ac
OlshaMB <michaelolshanskiy@icloud.com>
2023-06-24 21:14:12 +0300

    Adds: rio-canary cask

---
+++ Casks/rio-canary.rb
@@ -0,0 +1,10 @@
+require 'json'
+cask "rio-canary" do
+    version "0.0.8b_f841d78ea8c02e16b3b730be515056f169ebb44d"
+  
+    url "https://github.com/raphamorim/rio/releases/download/canary-f841d78ea8c02e16b3b730be515056f169ebb44d/macos-rio.zip"
+    name "Rio"
+    desc "A canry build of Rio"
+  
+    app "Rio.app"
+end
